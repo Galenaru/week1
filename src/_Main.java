c243547b4b2a207c4975f2ba7367363b3d2b5585
Alena_Gal <galenaru@yandex.ru>
2023-10-29 21:49:03 +0300

    first commit

---
+++ src/_Main.java
@@ -0,0 +1,14 @@
+
+public class _Main {
+    public static void main(String[] args){
+        introduceYourself();
+        setGoal();
+    }
+    public static void introduceYourself(){
+        System.out.println("Hello world! My name is Alyona.");
+    }
+    public static void setGoal(){
+       System.out.println("My goal is to complete the course and become a backend developer.");
+    }
+}
+
